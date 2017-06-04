79a76fb1f3db27593088e61d8ca7515425ad4b0c
jacobacollins <jacobacollins123@gmail.com>
2017-06-03 20:14:11 -0600

    Add files via upload

---
+++ ex29.rb
@@ -0,0 +1,50 @@
+#variables
+people = 20
+cats = 30
+dogs = 15
+
+#testing if statements
+#code block
+if people < cats
+	puts "Too many cats! The world is doomed!"
+end
+#end of code block
+
+#code block
+if people > cats
+	puts "Not many cats! The world is saved!"
+end
+#end of code block
+
+#code block
+if people < dogs
+	puts "The world is drooled on!"
+end
+#end of code block
+
+#code block
+if people > dogs
+	puts "The world is dry!"
+end
+#end of code block
+
+
+dogs += 5
+
+#code block
+if people >= dogs
+	puts "People are greater than or equal to dogs."
+end
+#end of code block
+
+#code block
+if people <= dogs
+	puts "People are less than or equal to dogs."
+end
+#end of code block
+
+#code block
+if people == dogs
+	puts "People are dogs"
+end
+#end of code block
---
+++ ex30.rb
@@ -0,0 +1,31 @@
+#varibles
+people = 30
+cars = 40
+trucks = 15
+
+#practice with if, else, and elsif statement
+#codeblock
+if cars > people
+	puts "We should take the cars."
+elsif cars < people
+	puts "We should not take the cars."
+else
+	puts "We can't decide."
+end
+#end of code block
+
+#code block
+if trucks > cars
+	puts "That's too many trucks."
+elsif trucks < cars
+	puts "Maybe we could take the trucks."
+else
+	puts "We still can't decide."
+end
+
+if people > trucks
+	puts "Alright, let's just take the trucks."
+else
+	puts "Fine, let's stay home then."
+end
+#end of code block
